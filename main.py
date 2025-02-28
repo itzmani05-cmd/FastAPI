e2e8e027310b637353ba7e52d74268e7e5c0899c
Manikandan <itzmanii2005@gmail.com>
2025-02-28 22:50:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello"}
+@app.get("/")  
+def home():
+    return {"message": "Welcome to FastAPI!"}
 
-my_posts = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "title of post 2", "content": "content of post 2", "id": 2},
-]
+@app.get("/message")  
+def say_hello(name: str):
+    return {"message": message}
 
-class Post(BaseModel):
-    title: str
-    content: str
-
-@app.post("/posts")
-def create_posts(post: Post):
-    new_post = {
-        "title": post.title,
-        "content": post.content,
-        "id": len(my_posts) + 1  
-    }
-    return {"data": new_post}
 
